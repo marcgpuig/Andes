59e123abac7861924c1293f24b47088e42b4ce54
Marc Garcia Puig <marcgpuig@gmail.com>
2018-01-10 00:28:56 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+## How to use
+
+### Windows
+
+Use the ```VsDevCmd.bat``` with ```-arch=x64```.
+	
+	git submodule init
+	git submodule update
+    mkdir build
+    cd build
+    cmake -G "NMake Makefiles" ..
+    nmake
+
---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <stdio.h>
+#include <iostream>
+
+namespace
+{
+   void errorCallback(int error, const char* description)
+   {
+      fprintf(stderr, "GLFW error %d: %s\n", error, description);
+   }
+
+   GLFWwindow* initialize()
+   {
+      int glfwInitRes = glfwInit();
+      if (!glfwInitRes)
+      {
+         fprintf(stderr, "Unable to initialize GLFW\n");
+         return nullptr;
+      }
+
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+      glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+      GLFWwindow* window = glfwCreateWindow(1280, 720, "InitGL", nullptr, nullptr);
+      if (!window)
+      {
+         fprintf(stderr, "Unable to create GLFW window\n");
+         glfwTerminate();
+         return nullptr;
+      }
+
+      glfwMakeContextCurrent(window);
+
+      int gladInitRes = gladLoadGL();
+      if (!gladInitRes)
+      {
+         fprintf(stderr, "Unable to initialize glad\n");
+         glfwDestroyWindow(window);
+         glfwTerminate();
+         return nullptr;
+      }
+
+      return window;
+   }
+}
+
+int main(int argc, char* argv[])
+{
+	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 trans;
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	vec = trans * vec;
+	std::cout << vec.x << vec.y << vec.z << std::endl;
+
+   glfwSetErrorCallback(errorCallback);
+
+   GLFWwindow* window = initialize();
+   if (!window)
+   {
+      return 0;
+   }
+
+   // Set the clear color to a nice green
+   glClearColor(0.5f, 0.2f, 0.8f, 1.0f);
+
+   while (!glfwWindowShouldClose(window))
+   {
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+   }
+
+   glfwDestroyWindow(window);
+   glfwTerminate();
+
+   return 0;
+}
